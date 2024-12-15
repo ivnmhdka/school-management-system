9bd6ec07f56a9a395faaf2bfadc135bf00d79154
Ivan Mahadika <ivanmahadika05@gmail.com>
2024-12-15 23:40:18 +0700

    create list

--- source.cpp
+++ source.cpp
@@ -1,1 +1,11 @@
 #include "header.h"
+
+void create_list_siswa(list_siswa &listSiswa) {
+    listSiswa.first = nullptr;
+    listSiswa.last = nullptr;
+}
+
+void create_list_guru(list_guru &listGuru) {
+    listGuru.first = nullptr;
+    listGuru.last = nullptr;
+}
